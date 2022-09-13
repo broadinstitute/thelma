97e0935c3fb65dc4f28af4d74301308499158798
mflinn-broad <mflinn@broadinstitute.org>
2022-09-13 12:18:30 -0400

    load state from sherlock wip

--- internal/thelma/clients/sherlock/state_loader.go
+++ internal/thelma/clients/sherlock/state_loader.go
@@ -8,37 +8,85 @@ import (
 )
 
 type StateLoader interface {
-	Environments() ([]*models.V2controllersEnvironment, error)
-	Clusters() ([]*models.V2controllersCluster, error)
-	ClusterReleases(clusterName string) ([]*models.V2controllersChartRelease, error)
-	EnvironmentReleases(environmentName string) ([]*models.V2controllersChartRelease, error)
+	Environments() (Environments, error)
+	Clusters() (Clusters, error)
+	ClusterReleases(clusterName string) (Releases, error)
+	EnvironmentReleases(environmentName string) (Releases, error)
 }
 
-func (c *Client) Environments() ([]*models.V2controllersEnvironment, error) {
+type Cluster struct {
+	*models.V2controllersCluster
+}
+
+type Clusters []Cluster
+
+type Environment struct {
+	*models.V2controllersEnvironment
+}
+
+type Environments []Environment
+
+type Release struct {
+	*models.V2controllersChartRelease
+}
+
+type Releases []Release
+
+func newEnvironments(envs ...*models.V2controllersEnvironment) Environments {
+	environments := make([]Environment, 0)
+	for _, env := range envs {
+		environments = append(environments, Environment{env})
+	}
+
+	return environments
+}
+
+func (c *Client) Environments() (Environments, error) {
 	params := environments.NewGetAPIV2EnvironmentsParams()
 	environmentsResponse, err := c.client.Environments.GetAPIV2Environments(params)
 	if err != nil {
 		return nil, err
 	}
 
-	environments := environmentsResponse.GetPayload()
+	environmentsPayload := environmentsResponse.GetPayload()
+
+	environments := newEnvironments(environmentsPayload...)
 
 	return environments, nil
 }
 
-func (c *Client) Clusters() ([]*models.V2controllersCluster, error) {
+func newClusters(cls ...*models.V2controllersCluster) Clusters {
+	clusters := make([]Cluster, 0)
+	for _, cluster := range cls {
+		clusters = append(clusters, Cluster{cluster})
+	}
+
+	return clusters
+}
+
+func (c *Client) Clusters() (Clusters, error) {
 	params := clusters.NewGetAPIV2ClustersParams()
 	clustersResponse, err := c.client.Clusters.GetAPIV2Clusters(params)
 	if err != nil {
 		return nil, err
 	}
 
-	clusters := clustersResponse.GetPayload()
+	clustersPayload := clustersResponse.GetPayload()
+	clusters := newClusters(clustersPayload...)
 
 	return clusters, nil
 }
 
-func (c *Client) ClusterReleases(clusterName string) ([]*models.V2controllersChartRelease, error) {
+func newReleases(rs ...*models.V2controllersChartRelease) Releases {
+	releases := make([]Release, 0)
+	for _, release := range rs {
+		releases = append(releases, Release{release})
+	}
+
+	return releases
+}
+
+func (c *Client) ClusterReleases(clusterName string) (Releases, error) {
 	params := chart_releases.NewGetAPIV2ChartReleasesParams()
 	params.Cluster = &clusterName
 
@@ -47,12 +95,13 @@ func (c *Client) ClusterReleases(clusterName string) ([]*models.V2controllersCha
 		return nil, err
 	}
 
-	clusterReleases := clusterReleasesResponse.GetPayload()
+	clusterReleasesPayload := clusterReleasesResponse.GetPayload()
+	clusterReleases := newReleases(clusterReleasesPayload...)
 
 	return clusterReleases, nil
 }
 
-func (c *Client) EnvironmentReleases(environmentName string) ([]*models.V2controllersChartRelease, error) {
+func (c *Client) EnvironmentReleases(environmentName string) (Releases, error) {
 	params := chart_releases.NewGetAPIV2ChartReleasesParams()
 	params.Environment = &environmentName
 
@@ -61,7 +110,8 @@ func (c *Client) EnvironmentReleases(environmentName string) ([]*models.V2contro
 		return nil, err
 	}
 
-	environmentReleases := environmentReleasesResponse.GetPayload()
+	environmentReleasesPayload := environmentReleasesResponse.GetPayload()
+	environmentReleases := newReleases(environmentReleasesPayload...)
 
 	return environmentReleases, nil
 }
--- internal/thelma/state/providers/sherlock/state_loader.go
+++ internal/thelma/state/providers/sherlock/state_loader.go
@@ -1,6 +1,7 @@
 package sherlock
 
 import (
+	"github.com/broadinstitute/sherlock/clients/go/client/models"
 	"github.com/broadinstitute/thelma/internal/thelma/clients/sherlock"
 	"github.com/broadinstitute/thelma/internal/thelma/state/api/terra"
 	"github.com/broadinstitute/thelma/internal/thelma/utils/shell"
@@ -21,5 +22,31 @@ func NewStateLoader(thelmaHome string, shellRunner shell.Runner, sherlock sherlo
 }
 
 func (s *stateLoader) Load() (terra.State, error) {
+	clusters, err := s.sherlock.Clusters()
+	if err != nil {
+		return nil, err
+	}
+
+	environments, err := s.sherlock.Environments()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, cluster := range clusters {
+		_, err := s.sherlock.ClusterReleases(cluster.Name)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for _, environment := range environments {
+		_, err := s.sherlock.EnvironmentReleases(environment.Name)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	panic("TODO")
 }
+
+func toStateEnvironment(environment []*models.V2controllersEnvironment) terra.Environment
