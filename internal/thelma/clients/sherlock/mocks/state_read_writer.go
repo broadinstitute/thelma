82dd1f16ec1382fa5d3e7eaf4e96d796e8f8853d
Jack Warren <jack@jackwarren.info>
2022-11-01 20:08:35 -0400

    rebuild mocks

--- internal/thelma/clients/sherlock/mocks/state_read_writer.go
+++ internal/thelma/clients/sherlock/mocks/state_read_writer.go
@@ -13,29 +13,6 @@ type StateReadWriter struct {
 	mock.Mock
 }
 
-// ClusterReleases provides a mock function with given fields: clusterName
-func (_m *StateReadWriter) ClusterReleases(clusterName string) (sherlock.Releases, error) {
-	ret := _m.Called(clusterName)
-
-	var r0 sherlock.Releases
-	if rf, ok := ret.Get(0).(func(string) sherlock.Releases); ok {
-		r0 = rf(clusterName)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(sherlock.Releases)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(string) error); ok {
-		r1 = rf(clusterName)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // Clusters provides a mock function with given fields:
 func (_m *StateReadWriter) Clusters() (sherlock.Clusters, error) {
 	ret := _m.Called()
@@ -129,29 +106,6 @@ func (_m *StateReadWriter) EnableRelease(_a0 terra.Environment, _a1 string) erro
 	}
 
 	return r0
-}
-
-// EnvironmentReleases provides a mock function with given fields: environmentName
-func (_m *StateReadWriter) EnvironmentReleases(environmentName string) (sherlock.Releases, error) {
-	ret := _m.Called(environmentName)
-
-	var r0 sherlock.Releases
-	if rf, ok := ret.Get(0).(func(string) sherlock.Releases); ok {
-		r0 = rf(environmentName)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(sherlock.Releases)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(string) error); ok {
-		r1 = rf(environmentName)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
 }
 
 // Environments provides a mock function with given fields:
