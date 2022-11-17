c8a61d82833c4feef4935973f2d8194682f073cd
Jack Warren <jack@jackwarren.info>
2022-11-17 17:39:17 -0500

    log configuration errors

--- internal/thelma/cli/commands/bee/common/builders/builders.go
+++ internal/thelma/cli/commands/bee/common/builders/builders.go
@@ -6,6 +6,7 @@ import (
 	"github.com/broadinstitute/thelma/internal/thelma/bee"
 	"github.com/broadinstitute/thelma/internal/thelma/bee/cleanup"
 	"github.com/broadinstitute/thelma/internal/thelma/bee/seed"
+	"github.com/rs/zerolog/log"
 )
 
 func NewBees(thelmaApp app.ThelmaApp) (bee.Bees, error) {
@@ -26,7 +27,11 @@ func NewBees(thelmaApp app.ThelmaApp) (bee.Bees, error) {
 		return nil, err
 	}
 
-	slack, _ := thelmaApp.Clients().Slack()
+	slack, err := thelmaApp.Clients().Slack()
+	if err != nil {
+		// Never error out on Slack issues, downstream calls are resilient to it having failed
+		log.Debug().Msgf("error configuring slack client: %v", err)
+	}
 
 	return bee.NewBees(_argocd, thelmaApp.StateLoader(), seeder, _cleanup, kubectl, slack)
 }
