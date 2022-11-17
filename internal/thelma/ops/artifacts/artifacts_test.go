a5104482810179e1820ca9e7be9ee75869294f4a
Chelsea Hoover <chelsea@broadinstitute.org>
2022-11-17 10:27:13 -0500

    aint happening

--- internal/thelma/ops/artifacts/artifacts_test.go
+++
@@ -1,7 +0,0 @@
-package artifacts
-
-import "testing"
-
-func TestArtifacts(t *testing.T) {
-	panic("TODO")
-}
--- internal/thelma/ops/logs/logs_test.go
+++
@@ -1,9 +0,0 @@
-package logs
-
-import (
-	"testing"
-)
-
-func Test_Export(t *testing.T) {
-	panic("TODO")
-}
