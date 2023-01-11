1f14332830bf3de035b79da81a5d71e7337ac36c
Chelsea Hoover <chelsea@broadinstitute.org>
2023-01-11 10:26:00 -0500

    add another retriable error

--- internal/thelma/clients/google/terraapi/firecloudorch.go
+++ internal/thelma/clients/google/terraapi/firecloudorch.go
@@ -26,6 +26,7 @@ var retryableErrors = []*regexp.Regexp{
 	regexp.MustCompile(`java\.net\.SocketTimeoutException`),
 	regexp.MustCompile(`java\.net\.UnknownHostException`),
 	regexp.MustCompile(`akka\.http\.impl\.engine\.client\.OutgoingConnectionBlueprint\$UnexpectedConnectionClosureException`),
+	regexp.MustCompile(`(?m)503 Service Temporarily Unavailable.*nginx`),
 }
 
 type FirecloudOrchClient interface {
