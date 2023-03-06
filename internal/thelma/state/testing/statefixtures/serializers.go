beb2d96f64222fd2cdc88c7f95efe837bc440d10
Chelsea Hoover <chelsea@broadinstitute.org>
2023-03-06 14:41:01 -0500

    Add new mock statefixture implementation

---
+++ internal/thelma/state/testing/statefixtures/serializers.go
@@ -0,0 +1,65 @@
+package statefixtures
+
+import (
+	"github.com/broadinstitute/thelma/internal/thelma/state/api/terra"
+)
+
+// FixtureData root type for a fixture definition file
+type FixtureData struct {
+	Clusters     []Cluster
+	Environments []Environment
+	Charts       []Chart
+	Releases     []Release
+}
+
+type Cluster struct {
+	Name             string
+	Base             string
+	Address          string
+	Project          string
+	Location         string
+	RequireSuitable  bool
+	TerraHelmfileRef string
+}
+
+type Environment struct {
+	Name                 string
+	Base                 string
+	Template             string
+	Lifecycle            terra.Lifecycle
+	UniqueResourcePrefix string
+	DefaultCluster       string
+	RequireSuitable      bool
+	TerraHelmfileRef     string
+}
+
+type Chart struct {
+	Name string
+	Repo string
+}
+
+type Release struct {
+	FullName            string
+	Repo                string
+	Chart               string
+	Cluster             string
+	Namespace           string
+	Environment         string
+	AppVersion          string
+	ChartVersion        string
+	TerraHelmfileRef    string
+	FirecloudDevelopRef string
+	Port                int
+	Protocol            string
+	Subdomain           string
+}
+
+func (r Release) key() string {
+	var dest string
+	if r.Environment != "" {
+		dest = r.Environment
+	} else {
+		dest = r.Cluster
+	}
+	return r.Chart + "-" + dest
+}
