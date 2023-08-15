1e68ee24f8d2dfd9cb2f2becd688a60009040fd6
Chelsea Hoover <60902147+choover-broad@users.noreply.github.com>
2023-08-15 16:00:31 -0400

    Replace a panic with a warning

--- internal/thelma/ops/status/event_matcher.go
+++ internal/thelma/ops/status/event_matcher.go
@@ -33,7 +33,8 @@ func newEventMatcher(apiClient kubernetes.Interface, namespace string) (*eventMa
 // annotateResource with any matching events
 func (e *eventMatcher) annotateResourceWithMatchingEvents(resource *Resource) error {
 	if resource.Namespace != e.namespace {
-		panic(fmt.Errorf("resource namespace %s does not match event cache namespace %s", resource.Namespace, e.namespace))
+		log.Warn().Msgf("Unexpected data inconsistency: resource namespace %s does not match event cache namespace %s", resource.Namespace, e.namespace)
+		return nil
 	}
 
 	var podSelector *metav1.LabelSelector
