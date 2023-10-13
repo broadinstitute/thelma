7447baadd043d1bd39c68042eaafdc872b43e7e5
Jack Warren <jack@jackwarren.info>
2023-10-13 15:57:40 -0400

    [DDO-3211] Use v3 endpoints, again, again (#174)

--- internal/thelma/clients/sherlock/chart_version_updater.go
+++ internal/thelma/clients/sherlock/chart_version_updater.go
@@ -14,22 +14,18 @@ import (
 type ChartVersionUpdater interface {
 	// UpdateForNewChartVersion does three things in sequence, all directly with Sherlock's API.
 	//
-	// First, it reports the new chart version to Sherlock. Sherlock will now most likely use that version as the
-	// new "latest" chart version, unless Sherlock's handling for replay-requests or concurrency control kicks in.
-	//
-	// If and only if that first step succeeds, each chart release is set to use the latest chart version. Sherlock
-	// automatically recalculates the latest version for each chart release, even if it was already set to use latest.
-	// Any version changes are applied automatically, assuming Thelma's authentication has sufficient access for the
-	// chart releases requested. If any part of this second step fails, the entire second step will have no effect.
-	//
-	// Lastly, Thelma looks up any template instances of this chart that are following the latest version of this chart,
-	// which would've just gotten updated. Thelma will issue a refresh for any such instances, so that directly
-	// template manifests will correctly have the new version.
+	// 1. Report new chart version to Sherlock (meaning there's a new latest chart version)
+	// 2. Update given chart releases (via chartReleaseSelectors) to point at the latest chart version and refresh to get the new latest version
+	//    - 9 times out of 10 this means `dev/${chartSelector}` based on how .autorelease.yaml file gets defaulted
+	// 3. Refresh **template** chart releases that either:
+	//    - already follow latest chart version (so step 1 means they'd have an update if we didn't catch them in step 2)
+	//    - follow a chart release we just updated in step 2 (so step 2 means they'd have an update)
 	UpdateForNewChartVersion(chartSelector string, newVersion string, lastVersion string, description string, chartReleaseSelectors ...string) error
 }
 
-func (c *Client) UpdateForNewChartVersion(chartSelector string, newVersion string, lastVersion string, description string, chartReleaseSelectors ...string) error {
-	chartVersion := &models.V2controllersCreatableChartVersion{
+// Step 1 of UpdateForNewChartVersion
+func (c *Client) reportNewChartVersion(chartSelector string, newVersion string, lastVersion string, description string) error {
+	chartVersion := &models.SherlockChartVersionV3Create{
 		Chart:        chartSelector,
 		ChartVersion: newVersion,
 		Description:  description,
@@ -37,11 +33,16 @@ func (c *Client) UpdateForNewChartVersion(chartSelector string, newVersion strin
 	if lastVersion != "" {
 		chartVersion.ParentChartVersion = fmt.Sprintf("%s/%s", chartSelector, lastVersion)
 	}
-	_, _, err := c.client.ChartVersions.PostAPIV2ChartVersions(
-		chart_versions.NewPostAPIV2ChartVersionsParams().WithChartVersion(chartVersion))
+	_, err := c.client.ChartVersions.PutAPIChartVersionsV3(
+		chart_versions.NewPutAPIChartVersionsV3Params().WithChartVersion(chartVersion))
 	if err != nil {
-		return errors.Errorf("error from Sherlock creating chart version %s/%s: %v", chartSelector, newVersion, err)
+		return errors.Errorf("error from Sherlock: %v", err)
 	}
+	return nil
+}
+
+// Step 2 of UpdateForNewChartVersion
+func (c *Client) setChartReleasesToLatestChartVersion(chartReleaseSelectors ...string) error {
 	var chartReleaseEntriesToUpdate []*models.V2controllersChangesetPlanRequestChartReleaseEntry
 	for _, chartReleaseSelector := range chartReleaseSelectors {
 		chartReleaseEntriesToUpdate = append(chartReleaseEntriesToUpdate, &models.V2controllersChangesetPlanRequestChartReleaseEntry{
@@ -52,26 +53,33 @@ func (c *Client) UpdateForNewChartVersion(chartSelector string, newVersion strin
 	changesetPlanRequest := &models.V2controllersChangesetPlanRequest{
 		ChartReleases: chartReleaseEntriesToUpdate,
 	}
-	_, _, err = c.client.Changesets.PostAPIV2ProceduresChangesetsPlanAndApply(
+	_, _, err := c.client.Changesets.PostAPIV2ProceduresChangesetsPlanAndApply(
 		changesets.NewPostAPIV2ProceduresChangesetsPlanAndApplyParams().WithChangesetPlanRequest(changesetPlanRequest))
 	if err != nil {
-		return errors.Errorf("error from Sherlock updating chart releases to new version %s/%s: %v", chartSelector, newVersion, err)
-	} else {
-		log.Info().Msgf("updated chart releases in Sherlock to new version %s/%s: %v", chartSelector, newVersion, chartReleaseSelectors)
+		return errors.Errorf("error from Sherlock: %v", err)
 	}
+	return nil
+}
 
+// Step 3 of UpdateForNewChartVersion
+func (c *Client) refreshDownstreamTemplateChartReleases(chartSelector string, updatedChartReleases ...string) (refreshedChartReleases []string, err error) {
+	// Get list of template environments
 	templateString := "template"
 	templates, err := c.client.Environments.GetAPIV2Environments(
 		environments.NewGetAPIV2EnvironmentsParams().
 			WithLifecycle(&templateString),
 	)
 	if err != nil {
-		return errors.Errorf("error from Sherlock getting template environments: %v", err)
+		return []string{}, errors.Errorf("error from Sherlock getting template environments: %v", err)
 	}
+
+	// Assemble list of all the downstream template chart releases we should update
 	var chartReleasesToRefresh []string
 	latestString := "latest"
 	followString := "follow"
 	for _, template := range templates.Payload {
+		// First, get applicable template chart releases that are currently following the latest chart version (that
+		// we just updated)
 		chartReleasesUsingLatest, err := c.client.ChartReleases.GetAPIV2ChartReleases(
 			chart_releases.NewGetAPIV2ChartReleasesParams().
 				WithChart(&chartSelector).
@@ -79,13 +87,16 @@ func (c *Client) UpdateForNewChartVersion(chartSelector string, newVersion strin
 				WithChartVersionResolver(&latestString),
 		)
 		if err != nil {
-			return errors.Errorf("error from Sherlock getting latest chart releases in template %s: %v", template.Name, err)
+			return []string{}, errors.Errorf("error from Sherlock getting latest chart releases in template %s: %v", template.Name, err)
 		} else {
 			for _, chartRelease := range chartReleasesUsingLatest.Payload {
 				chartReleasesToRefresh = append(chartReleasesToRefresh, chartRelease.Name)
 			}
 		}
-		for _, chartReleaseThatGotUpdated := range chartReleaseSelectors {
+
+		// Second, get applicable template chart releases that are currently following a chart release we just specifically
+		// updated
+		for _, chartReleaseThatGotUpdated := range updatedChartReleases {
 			chartReleasesUsingFollow, err := c.client.ChartReleases.GetAPIV2ChartReleases(
 				chart_releases.NewGetAPIV2ChartReleasesParams().
 					WithChart(&chartSelector).
@@ -94,7 +105,7 @@ func (c *Client) UpdateForNewChartVersion(chartSelector string, newVersion strin
 					WithChartVersionFollowChartRelease(&chartReleaseThatGotUpdated),
 			)
 			if err != nil {
-				return errors.Errorf("error from Sherlock getting chart releases following %s in template %s: %v", chartReleaseThatGotUpdated, template.Name, err)
+				return []string{}, errors.Errorf("error from Sherlock getting chart releases following %s in template %s: %v", chartReleaseThatGotUpdated, template.Name, err)
 			} else {
 				for _, chartRelease := range chartReleasesUsingFollow.Payload {
 					chartReleasesToRefresh = append(chartReleasesToRefresh, chartRelease.Name)
@@ -103,6 +114,7 @@ func (c *Client) UpdateForNewChartVersion(chartSelector string, newVersion strin
 		}
 	}
 
+	// Create a changeset request to just refresh every chart release we collected
 	if len(chartReleasesToRefresh) > 0 {
 		var chartReleaseEntriesToRefresh []*models.V2controllersChangesetPlanRequestChartReleaseEntry
 		for _, chartReleaseSelector := range chartReleasesToRefresh {
@@ -110,18 +122,36 @@ func (c *Client) UpdateForNewChartVersion(chartSelector string, newVersion strin
 				ChartRelease: chartReleaseSelector,
 			})
 		}
-		changesetPlanRequest = &models.V2controllersChangesetPlanRequest{
+		changesetPlanRequest := &models.V2controllersChangesetPlanRequest{
 			ChartReleases: chartReleaseEntriesToRefresh,
 		}
 		_, _, err = c.client.Changesets.PostAPIV2ProceduresChangesetsPlanAndApply(
 			changesets.NewPostAPIV2ProceduresChangesetsPlanAndApplyParams().WithChangesetPlanRequest(changesetPlanRequest))
 		if err != nil {
-			return errors.Errorf("error from Sherlock refreshing template chart releases to reflect new version %s/%s: %v", chartSelector, newVersion, err)
-		} else {
-			log.Info().Msgf("refreshed template chart releases in Sherlock to reflect new version %s/%s: %v", chartSelector, newVersion, chartReleasesToRefresh)
+			return []string{}, errors.Errorf("error from Sherlock: %v", err)
 		}
+	}
+
+	return chartReleasesToRefresh, nil
+}
+
+func (c *Client) UpdateForNewChartVersion(chartSelector string, newVersion string, lastVersion string, description string, chartReleaseSelectors ...string) error {
+	if err := c.reportNewChartVersion(chartSelector, newVersion, lastVersion, description); err != nil {
+		return errors.Errorf("error reporting chart version %s/%s: %v", chartSelector, newVersion, err)
+	}
+
+	if err := c.setChartReleasesToLatestChartVersion(chartReleaseSelectors...); err != nil {
+		return errors.Errorf("error setting chart releases to latest chart version (%s/%s): %v", chartSelector, newVersion, err)
 	} else {
-		log.Info().Msg("no template chart releases to refresh")
+		log.Info().Msgf("updated chart releases in Sherlock to new version %s/%s: %v", chartSelector, newVersion, chartReleaseSelectors)
+	}
+
+	if refreshedChartReleases, err := c.refreshDownstreamTemplateChartReleases(chartSelector, chartReleaseSelectors...); err != nil {
+		return errors.Errorf("error refreshing downstream template chart releases after reporting new chart version (%s/%s) and updating the following direct chart releases (%v): %v", chartSelector, newVersion, chartReleaseSelectors, err)
+	} else if len(refreshedChartReleases) > 0 {
+		log.Info().Msgf("updated further downstream template chart releases in Sherlock to reflect new version %s/%s: %v", chartSelector, newVersion, refreshedChartReleases)
+	} else {
+		log.Info().Msgf("no further downstream template chart releases in Sherlock to update to reflect new version %s/%s", chartSelector, newVersion)
 	}
 
 	return nil
