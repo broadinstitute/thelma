101d4ddfb55b9b3156663bda2c7bb86d9aec625e
Jack Warren <jack@jackwarren.info>
2023-11-13 09:58:05 -0500

    parallelize test user registration

--- internal/thelma/bee/seed/seed_step_4_register_test_users.go
+++ internal/thelma/bee/seed/seed_step_4_register_test_users.go
@@ -1,7 +1,10 @@
 package seed
 
 import (
+	"github.com/broadinstitute/thelma/internal/thelma/clients/google"
+	"github.com/broadinstitute/thelma/internal/thelma/clients/google/terraapi"
 	"github.com/broadinstitute/thelma/internal/thelma/state/api/terra"
+	"github.com/broadinstitute/thelma/internal/thelma/utils/pool"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 )
@@ -23,33 +26,61 @@ func (s *seeder) seedStep4RegisterTestUsers(appReleases map[string]terra.AppRele
 				return errors.Errorf("suffix %s of project %s maps to no test users", sam.Cluster().ProjectSuffix(), sam.Cluster().Project())
 			}
 
-			googleClient, err := s.googleAuthAs(orch)
-			if err != nil {
-				return err
+			var jobs []pool.Job
+			for _, unsafe := range users {
+				user := unsafe
+				jobs = append(jobs, pool.Job{
+					Name: user.Email,
+					Run: func(reporter pool.StatusReporter) error {
+						var err error
+						reporter.Update(pool.Status{
+							Message: "Authenticating",
+						})
+						var googleClient google.Clients
+						googleClient, err = s.googleAuthAs(orch)
+						if err = opts.handleErrorWithForce(err); err != nil {
+							return err
+						}
+						var terraClient terraapi.TerraClient
+						terraClient, err = googleClient.SetSubject(user.Email).Terra()
+						if err = opts.handleErrorWithForce(err); err != nil {
+							return err
+						}
+						terraClient.SetPoolStatusReporter(reporter)
+						reporter.Update(pool.Status{
+							Message: "Registering",
+						})
+						_, _, err = terraClient.FirecloudOrch(orch).RegisterProfile(
+							user.FirstName, user.LastName, user.Role, user.Email,
+							"Hogwarts", "dsde",
+							"Cambridge", "MA", "USA",
+							"Remus Lupin", "Non-Profit")
+						if err = opts.handleErrorWithForce(err); err != nil {
+							return err
+						}
+						reporter.Update(pool.Status{
+							Message: "Approving TOS",
+						})
+						_, _, err = terraClient.Sam(sam).AcceptToS()
+						if err = opts.handleErrorWithForce(err); err != nil {
+							return err
+						}
+						reporter.Update(pool.Status{
+							Message: "Registered",
+						})
+						return nil
+					},
+				})
 			}
 
-			for index, user := range users {
-				log.Info().Msgf("User %d - %s - registering", index+1, user.Email)
-				terraClient, err := googleClient.SetSubject(user.Email).Terra()
-				if err != nil {
-					if opts.handleErrorWithForce(err) != nil {
-						return err
-					}
-					continue
-				}
-				_, _, err = terraClient.FirecloudOrch(orch).RegisterProfile(
-					user.FirstName, user.LastName, user.Role, user.Email,
-					"Hogwarts", "dsde",
-					"Cambridge", "MA", "USA",
-					"Remus Lupin", "Non-Profit")
-				if err = opts.handleErrorWithForce(err); err != nil {
-					return err
-				}
-				log.Info().Msgf("User %d - %s - approving Terms of Service", index+1, user.Email)
-				_, _, err = terraClient.Sam(sam).AcceptToS()
-				if err = opts.handleErrorWithForce(err); err != nil {
-					return err
-				}
+			err = pool.New(jobs, func(o *pool.Options) {
+				o.NumWorkers = opts.RegistrationParallelism
+				o.Summarizer.Enabled = true
+				o.Metrics.Enabled = false
+				o.StopProcessingOnError = !opts.Force
+			}).Execute()
+			if err = opts.handleErrorWithForce(err); err != nil {
+				return err
 			}
 		} else {
 			log.Info().Msg("Sam not present in environment, skipping all")
